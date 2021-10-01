d4459251476d43701e58196105f2195a7b5ed30e
Khadija Al-Khashab <khadija.alkhashab@Khadija.local>
2021-10-01 07:44:27 +0300

    my task solution

--- task.js
+++ task.js
@@ -1,42 +1,35 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+let numbers = [ 5, 8, 0, 1, 9, 11, 15, 16 ];
 
 console.log("Original numbers list: ", numbers)
 
 for(let i = 0 ; i < numbers.length ; i++)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+  for(let j = 0 ; j < numbers.length - 1 ; j++)
+  {
+    // compare item with the whole array including itself
+    if(numbers[i] < numbers[j])
     {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
+      let temp = numbers[i];
+      numbers[i] = numbers[j]
+      numbers[j] = temp;
     }
+  }
 }
 
 console.log("Numbers list After sorting: ", numbers)
 
 for(let i = 0 ; i < numbers.length ; i++)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+  for(let j = 0 ; j < numbers.length - 1 ; j++)
+  {
+    // compare item with the whole array including itself
+    if(numbers[i] > numbers[j])
     {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
+      let temp = numbers[j];
+      numbers[j] = numbers[i]
+      numbers[i] = temp;
     }
+  }
 }
 
 console.log("Numbers list After Desc sorting: ", numbers)
